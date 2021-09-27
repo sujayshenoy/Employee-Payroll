807c1f5ad18447efa7d060682e97927461c27ec5
Sujay S Shenoy <sujay.shenoy@ymedialabs.com>
2021-09-27 22:47:24 +0530

    reading and writing employee details from console , to console

--- src/com/yml/employeepayroll/EmployeePayrollMain.java
+++ src/com/yml/employeepayroll/EmployeePayrollMain.java
@@ -1,7 +1,28 @@
 package com.yml.employeepayroll;
 
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class EmployeePayrollMain {
     public static void main(String args[]) {
-        
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out,true);
+
+        out.println("Enter Employee ID: ");
+        int id = in.nextInt();
+        in.nextLine();
+        out.println("Enter Employee Name: ");
+        String name = in.nextLine();
+        out.println("Enter Employee Salary: ");
+        double salary = in.nextDouble();
+        EmployeePayroll payroll = new EmployeePayroll(id, name, salary);
+
+        out.println("Entered Data: ");
+        out.println("Employee ID : "+ payroll.getId());
+        out.println("Employee Name : "+ payroll.getName());
+        out.println("Employee Salary : " + payroll.getSalary());
+
+        out.close();
+        in.close();
     }
 }
